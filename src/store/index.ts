d58e5128093cca9b3d0a08145eb6196b378aa650
wangchao <magician.of.technique@gmail.com>
2021-01-14 16:49:41 +0800

    [*] 修改类型引入错误问题

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { InjectionKey } from "vue";
 import { createStore, useStore as baseUseStore, Store } from "vuex";
 import modules from "./module";
-import { AllState, RootState } from "./types";
+import { AllState, RootState } from "@/types";
 
 export const store = createStore<RootState>({
   state: {
