b8da0ec75a59dc28d810ccc880879775d7e088df
wangchao <magician.of.technique@gmail.com>
2021-01-18 13:48:48 +0800

    [*] 优化项目结构

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,10 @@
 import { App } from "vue";
-import IngotAppBar from "./IngotAppBar.vue";
-import IngotSidebarMenu from "./sidebar/IngotSidebarMenu.vue";
-import IngotIcon from "./IngotIcon.vue";
+import { registerCoreComponent } from "@/core/component";
 
 /**
  * 注册组件
  * @param app App
  */
 export function registerComponent(app: App) {
-  app.component("ingot-app-bar", IngotAppBar);
-  app.component("ingot-sidebar-menu", IngotSidebarMenu);
-  app.component("ingot-icon", IngotIcon);
+  registerCoreComponent(app);
 }
---
+++ src/core/component/index.ts
@@ -0,0 +1,14 @@
+import { App } from "vue";
+import IngotAppBar from "./IngotAppBar.vue";
+import IngotSidebarMenu from "./sidebar/IngotSidebarMenu.vue";
+import IngotIcon from "./IngotIcon.vue";
+
+/**
+ * 注册组件
+ * @param app App
+ */
+export function registerCoreComponent(app: App) {
+  app.component("ingot-app-bar", IngotAppBar);
+  app.component("ingot-sidebar-menu", IngotSidebarMenu);
+  app.component("ingot-icon", IngotIcon);
+}
