f83428f8a73db1a6cf111ddc7376a8468a940036
wangchao <magician.of.technique@gmail.com>
2021-01-19 17:09:14 +0800

    [*] 增加 refreshToken 方法

--- src/core/api/auth.ts
+++ src/core/api/auth.ts
@@ -5,6 +5,26 @@ import { UserToken } from "@/core/model";
 const BasicToken = "Basic d2ViLWNsb3VkOndlYi1jbG91ZA==";
 
 /**
+ * 刷新Token
+ * @param refreshToken
+ */
+export function refreshToken(refreshToken: string) {
+  const data = Qs.stringify({
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    refresh_token: refreshToken,
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    grant_type: "refresh_token",
+    scope: "web"
+  });
+  return request.post<UserToken>("/api/acs/oauth/token", data, {
+    headers: {
+      Tenant: "1",
+      Authorization: BasicToken
+    }
+  });
+}
+
+/**
  * 通过密码登录
  * @param param0
  */
