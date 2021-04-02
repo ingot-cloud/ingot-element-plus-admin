bbed897090f8055385f74f8d16cf56e41283158f
wangchao <magician.of.technique@gmail.com>
2021-04-02 18:09:04 +0800

    [+] 增加 IngotContainer

---
+++ src/core/component/IngotContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+
+<style scoped lang="stylus">
+.container
+  margin 10px
+  background white
+</style>
