01dc664db940bcd7d162559c6c74d1a39613d62b
wangchao <magician.of.technique@gmail.com>
2021-04-06 16:04:46 +0800

    [*] 修改布局

--- src/views/management/user/index.vue
+++ src/views/management/user/index.vue
@@ -1,24 +1,30 @@
 <template>
   <ingot-container>
-    <el-row>
-      <el-col :span="4">
-        <el-tree
-          :data="deptData.data"
-          :props="deptData.props"
-          node-key="id"
-          :default-expanded-keys="[1]"
-        ></el-tree>
-      </el-col>
-      <el-col :span="20">
-        <el-table :data="userData.records">
-          <el-table-column prop="date" label="日期" width="180">
-          </el-table-column>
-          <el-table-column prop="name" label="姓名" width="180">
-          </el-table-column>
-          <el-table-column prop="address" label="地址"> </el-table-column>
-        </el-table>
-      </el-col>
-    </el-row>
+    <el-card shadow="nerver">
+      <el-row>
+        <el-col :span="4">
+          <el-tree
+            :data="deptData.data"
+            :props="deptData.props"
+            node-key="id"
+            :default-expanded-keys="[1]"
+            :highlight-current="true"
+            :expand-on-click-node="false"
+          ></el-tree>
+        </el-col>
+        <el-col :span="20">
+          <el-card>
+            <el-table :data="userData.records">
+              <el-table-column prop="date" label="日期" width="180">
+              </el-table-column>
+              <el-table-column prop="name" label="姓名" width="180">
+              </el-table-column>
+              <el-table-column prop="address" label="地址"> </el-table-column>
+            </el-table>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-card>
   </ingot-container>
 </template>
 
