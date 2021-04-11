7e5082e3b861b19f42951872a378e52f456ad2d6
wangchao <magician.of.technique@gmail.com>
2021-04-11 18:17:14 +0800

    [*] edit

--- src/views/authority/dept/composition/edit.ts
+++ src/views/authority/dept/composition/edit.ts
@@ -1,5 +1,7 @@
 import { reactive, ref } from "vue";
-import { CommonStatus, DeptRoleScope } from "@/model";
+import { CommonStatus, DeptRoleScope, DeptTreeNode } from "@/model";
+import { selectedDeptNode } from "./dept";
+import { ElMessage } from "element-plus";
 
 export const deptEditStatus = reactive({
   saveOrUpdateButtonTitle: "添加",
@@ -16,6 +18,17 @@ export const formModel = reactive({
   status: CommonStatus.Enable
 });
 
+// 重置 form model
+function resetFormModel(node?: DeptTreeNode) {
+  formModel.id = node?.id || "";
+  formModel.pid = node?.pid || "";
+  formModel.name = node?.name || "";
+  formModel.scope = node?.scope || DeptRoleScope.CurrentChild;
+  formModel.sort = node?.sort || 0;
+  formModel.status = node?.status || CommonStatus.Enable;
+}
+
+// 表达规则
 export const rules = ref({
   name: [{ required: true, message: "请输入部门名称", trigger: "blur" }],
   scope: [{ required: true, message: "请选择角色范围", trigger: "blur" }],
@@ -26,11 +39,22 @@ export const rules = ref({
 export function handleCreateButtonClick() {
   deptEditStatus.formDisabled = false;
   deptEditStatus.saveOrUpdateButtonTitle = "添加";
+  resetFormModel();
 }
 
 export function handleEditButtonClick() {
+  const keys = Object.keys(selectedDeptNode.value);
+  if (keys.length === 0) {
+    ElMessage({
+      showClose: true,
+      message: "请先选择部门",
+      type: "warning"
+    });
+    return;
+  }
   deptEditStatus.formDisabled = false;
   deptEditStatus.saveOrUpdateButtonTitle = "更新";
+  resetFormModel(selectedDeptNode.value);
 }
 
 export function handleDeleteButtonClick() {
@@ -41,7 +65,7 @@ export function handleDeleteButtonClick() {
  * 处理添加或者更新部门信息
  */
 export function handleCreateOrUpdateDept() {
-  //
+  resetFormModel();
 }
 
 /**
@@ -49,4 +73,6 @@ export function handleCreateOrUpdateDept() {
  */
 export function handleCancelEdit() {
   deptEditStatus.formDisabled = true;
+
+  resetFormModel();
 }
