d99a2d07430f0d0d31fd93da0111a2bca35cc53a
wangchao <magician.of.technique@gmail.com>
2021-05-23 17:20:10 +0800

    [*] 部门api

--- src/api/authority/dept.ts
+++ src/api/authority/dept.ts
@@ -9,3 +9,33 @@ export function getDeptTree() {
     url: "/api/pms/v1/dept/tree"
   });
 }
+
+/**
+ * 创建部门
+ */
+export function createDept(params: SysDept) {
+  return request.post({
+    url: "/api/pms/v1/dept",
+    params
+  });
+}
+
+/**
+ * 更新部门信息
+ */
+export function updateDept(params: SysDept) {
+  return request.put({
+    url: "/api/pms/v1/dept",
+    params
+  });
+}
+
+/**
+ * 删除部门
+ * @param id 部门id
+ */
+export function removeDept(id: string) {
+  return request.delete({
+    url: `/api/pms/v1/dept/${id}`
+  });
+}
