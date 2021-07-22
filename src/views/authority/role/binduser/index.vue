2b7635fe4d674e80ac29ee3791d66a4811f51c75
wangchao <magician.of.technique@gmail.com>
2021-07-22 16:50:55 +0800

    [*] temp

--- src/views/authority/role/binduser/index.vue
+++ src/views/authority/role/binduser/index.vue
@@ -2,8 +2,16 @@
   <ingot-container>
     <ingot-page-card @back="$router.back()" :content="title">
       <el-row :gutter="20">
-        <el-col :span="12">1</el-col>
-        <el-col :span="12">2</el-col>
+        <el-col :span="12">
+          <el-card>
+            <template #header> 已绑用户 </template>
+          </el-card>
+        </el-col>
+        <el-col :span="12">
+          <el-card>
+            <template #header> 所有用户 </template>
+          </el-card>
+        </el-col>
       </el-row>
     </ingot-page-card>
   </ingot-container>
