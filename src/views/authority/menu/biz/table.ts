95c113ff9ee0cbab14bd79d8566b01ed7f54f440
wangchao <magician.of.technique@gmail.com>
2021-07-23 17:50:37 +0800

    [*] menu重构，增加 table.ts

---
+++ src/views/authority/menu/biz/table.ts
@@ -0,0 +1,50 @@
+export const tableHeaders = [
+  {
+    label: "序号",
+    prop: "id",
+    type: "index",
+  },
+  {
+    label: "菜单名称",
+    prop: "name",
+  },
+  {
+    label: "图标",
+    prop: "icon",
+  },
+  {
+    label: "路径",
+    prop: "path",
+  },
+  {
+    label: "视图路径",
+    prop: "viewPath",
+  },
+  {
+    label: "排序",
+    prop: "sort",
+  },
+  {
+    label: "是否隐藏",
+    prop: "hidden",
+  },
+  {
+    label: "是否缓存",
+    prop: "isCache",
+  },
+  {
+    label: "状态",
+    prop: "status",
+  },
+  {
+    label: "备注",
+    prop: "remark",
+  },
+  {
+    label: "操作",
+    width: "210",
+    prop: "actions",
+    fixed: "right",
+    align: "center",
+  },
+];
