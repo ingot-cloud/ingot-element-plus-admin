bb2693ce9c9bf3cf2f948cd2a96de9cce5c9e237
wangchao <magician.of.technique@gmail.com>
2021-08-23 17:51:17 +0800

    [*] dept 使用 script setup

--- src/views/authority/dept/index.vue
+++ src/views/authority/dept/index.vue
@@ -55,5 +55,59 @@
   </ingot-container>
   <EditDialog ref="editDialogRef" :data="selectData" @success="fetchData" />
 </template>
-<script lang="ts" src="./dept.ts"></script>
+<script lang="ts" setup>
+import { onMounted, ref, unref } from "vue";
+import { tableHeaders } from "./table";
+import { DeptTreeNode, SysDept } from "@/model";
+import { useStore } from "@/store";
+import {
+  computedDeptTreeListData,
+  fetchDeptTree,
+  removeDept,
+} from "@/store/composition/dept";
+import { Confirm, Message } from "@/utils/message";
+import EditDialog from "./EditDialog.vue";
+
+const store = useStore();
+const editDialogRef = ref();
+const loading = ref(false);
+const treeData = computedDeptTreeListData();
+const selectData = ref([] as Array<DeptTreeNode>);
+
+onMounted(() => {
+  fetchData();
+});
+
+const fetchData = () => {
+  loading.value = true;
+  fetchDeptTree(store)
+    .then((data) => {
+      loading.value = false;
+      selectData.value = [
+        {
+          id: undefined,
+          name: "根部门",
+          children: data,
+        },
+      ];
+    })
+    .catch(() => {
+      loading.value = false;
+    });
+};
+
+const showEditDialog = (params?: SysDept | string) => {
+  const dialog = unref(editDialogRef);
+  dialog.show(params);
+};
+
+const handleDelete = (params: SysDept) => {
+  Confirm.warning(`是否删除部门${params.name}`).then(() => {
+    removeDept(store, params.id as string).then(() => {
+      Message.success("操作成功");
+      fetchData();
+    });
+  });
+};
+</script>
 <style lang="stylus" scoped></style>
