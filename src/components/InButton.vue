7c897c1d32749d63006af0498fcc33086b16f64f
wangchao <magician.of.technique@gmail.com>
2022-12-21 08:46:13 +0800

    [*] InButton 增加InClick事件，避免重复触发点击事件

--- src/components/InButton.vue
+++ src/components/InButton.vue
@@ -1,6 +1,16 @@
 <template>
-  <el-button>
-    <span> <slot name="icon"> </slot> </span>
+  <el-button @click="privateOnClick">
+    <span v-if="slots.icon"> <slot name="icon"> </slot> </span>
     <slot></slot>
   </el-button>
 </template>
+<script lang="ts" setup>
+const slots = useSlots();
+const emits = defineEmits(["in-click"]);
+const triggerClick = useThrottleFn((e: MouseEvent) => {
+  emits("in-click", e);
+}, 1500);
+const privateOnClick = (e: MouseEvent) => {
+  triggerClick(e);
+};
+</script>
