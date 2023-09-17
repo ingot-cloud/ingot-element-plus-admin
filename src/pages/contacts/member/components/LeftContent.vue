fa105ec67115c0f1c155e6d26254a68849db145a
wangchao <magician.of.technique@gmail.com>
2023-09-17 12:35:19 +0800

    [*] 搜索栏

--- src/pages/contacts/member/components/LeftContent.vue
+++ src/pages/contacts/member/components/LeftContent.vue
@@ -1,2 +1,19 @@
-<template>aa</template>
-<script setup lang="ts"></script>
+<template>
+  <div class="dept-filter">
+    <el-input
+      v-model="searchValue"
+      placeholder="搜索部门"
+      :prefix-icon="Search"
+      clearable
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { Search } from "@element-plus/icons-vue";
+const searchValue = ref("");
+</script>
+<style scoped lang="postcss">
+.dept-filter {
+  @apply flex flex-col;
+}
+</style>
--- src/pages/contacts/role/components/LeftContent.vue
+++ src/pages/contacts/role/components/LeftContent.vue
@@ -1,2 +1,19 @@
-<template>aa</template>
-<script setup lang="ts"></script>
+<template>
+  <div class="role-filter">
+    <el-input
+      v-model="searchValue"
+      placeholder="搜索角色"
+      :prefix-icon="Search"
+      clearable
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { Search } from "@element-plus/icons-vue";
+const searchValue = ref("");
+</script>
+<style scoped lang="postcss">
+.role-filter {
+  @apply flex flex-col;
+}
+</style>
