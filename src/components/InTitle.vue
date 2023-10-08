9b12f55b5f91c0c505f2cb52b4df875015c8bebb
wangchao <magician.of.technique@gmail.com>
2023-10-08 11:46:48 +0800

    [+] 增加InTitle，默认样式

---
+++ src/components/InTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <span class="in-title">{{ title }}</span>
+</template>
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<style lang="postcss" scoped>
+.in-title {
+  color: var(--in-common-title-color);
+  font-weight: var(--in-common-title-font-weight);
+  font-size: var(--in-common-title-font-size);
+}
+</style>
