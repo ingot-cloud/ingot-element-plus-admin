360e7039bc5691a2cceb453db8133718c20a3e2a
wangchao <magician.of.technique@gmail.com>
2023-10-25 09:59:43 +0800

    [+] 增加useImageLoad

---
+++ src/hooks/components/useImageLoad.ts
@@ -0,0 +1,36 @@
+import type { Ref } from "vue";
+import { onMounted } from "vue";
+
+const options = {
+  // root: document.querySelector(".container"), // 根元素，默认为视口
+  rootMargin: "0px", // 根元素的边距
+  threshold: 0.5, // 可见性比例阈值
+  once: true,
+};
+
+function callback(
+  entries: IntersectionObserverEntry[],
+  observer: IntersectionObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio <= 0) return;
+    const img = entry.target as HTMLImageElement;
+    const src = img.getAttribute("load-src");
+
+    img.setAttribute("src", src ?? ""); // 将真实的图片地址赋给 src 属性
+
+    img.onload = () => {
+      img.setAttribute("class", "in-anim-fade-in");
+    };
+
+    observer.unobserve(img);
+  });
+}
+
+export const useImageLoad = (ref: Ref) => {
+  const observer = new IntersectionObserver(callback, options);
+
+  onMounted(() => {
+    Object.keys(ref.value).forEach((e) => observer.observe(ref.value[e]));
+  });
+};
