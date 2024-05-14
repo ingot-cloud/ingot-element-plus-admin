8e31dc4334f4a44babfb548189b379dc2a6b827f
jy <magician.of.technique@aliyun.com>
2024-05-14 16:40:54 +0800

    [*] 内嵌链接优化

--- src/components/container/InContainer.vue
+++ src/components/container/InContainer.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="in-container">
+  <div
+    class="in-container"
+    :style="{ padding: `${padding}`, borderRadius: `${radius}` }"
+  >
     <el-backtop
       v-if="showBacktop"
       target=".in-container"
@@ -10,7 +13,9 @@
         <i-material-symbols:vertical-align-top-rounded />
       </div>
     </el-backtop>
-    <slot />
+    <div h-full w-full ref="ContentRef">
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -20,12 +25,30 @@ defineProps({
     type: Boolean,
     default: true,
   },
+  padding: {
+    type: String,
+    default: "var(--in-common-padding)",
+  },
+  radius: {
+    type: String,
+    default: "var(--in-common-border-radius)",
+  },
+});
+
+const ContentRef = ref();
+
+defineExpose({
+  getContentSize() {
+    return {
+      height: getComputedStyle(ContentRef.value).height,
+      width: getComputedStyle(ContentRef.value).width,
+    };
+  },
 });
 </script>
 
 <style lang="postcss" scoped>
 .in-container {
-  @apply w-full h-full box-border overflow-x-hidden p-[var(--in-common-padding)] shadow-sm bg-[var(--in-bg-color)];
-  border-radius: var(--in-common-border-radius);
+  @apply w-full h-full box-border overflow-x-hidden shadow-sm bg-[var(--in-bg-color)];
 }
 </style>
--- src/layouts/InIFrameLayout.vue
+++ src/layouts/InIFrameLayout.vue
@@ -1,17 +1,102 @@
 <template>
-  <in-container>
-    <!-- <iframe
-    :src="v.meta.isLink"
-    :key="v.path"
-    frameborder="0"
-    height="100%"
-    width="100%"
-    style="position: absolute"
-    :data-url="v.path"
-    v-show="getRoutePath === v.path"
-    ref="iframeRef"
-  /> -->
-    内嵌页面1
-    <div>a</div>
+  <in-container ref="ContainerRef">
+    <div class="in-frame-layout">
+      <div
+        w-full
+        h-full
+        v-for="item in urlArray"
+        :key="item.path"
+        class="in-frame-box"
+      >
+        <iframe
+          :src="item.url"
+          :key="item.path"
+          frameborder="0"
+          :height="frameHeight"
+          :width="frameWidth"
+          :data-url="item.path"
+          v-show="getRoutePath === item.path"
+          ref="IFrameRef"
+        />
+      </div>
+    </div>
   </in-container>
 </template>
+<script setup lang="ts">
+interface PathURL {
+  path: string;
+  url: string;
+  loading: boolean;
+}
+
+const urlArray = ref<Array<PathURL>>([]);
+const route = useRoute();
+
+const ContainerRef = ref();
+const IFrameRef = ref();
+const frameHeight = ref("0");
+const frameWidth = ref("0");
+
+const getRoutePath = computed(() => {
+  return route.fullPath;
+});
+
+const setIFrameSize = () => {
+  if (frameHeight.value == "0" || frameWidth.value == "0") {
+    nextTick(() => {
+      const size = ContainerRef.value.getContentSize();
+      frameHeight.value = size.height;
+      frameWidth.value = size.width;
+    });
+  }
+};
+
+onMounted(() => {
+  setIFrameSize();
+});
+
+watch(
+  () => route.fullPath,
+  (fullPath) => {
+    setIFrameSize();
+    let current = urlArray.value.find((item) => {
+      return item.path === route.fullPath;
+    });
+    if (!current) {
+      current = {
+        path: route.fullPath,
+        url: route.meta.linkURL!,
+        loading: false,
+      };
+      urlArray.value.push(current);
+    }
+
+    nextTick(() => {
+      if (!IFrameRef.value) {
+        return;
+      }
+      IFrameRef.value.forEach((v: HTMLElement) => {
+        if (v.dataset.url === fullPath) {
+          v.onload = () => {
+            if (current.loading) current.loading = false;
+          };
+        }
+      });
+    });
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
+<style scoped lang="postcss">
+.in-frame-layout {
+  position: relative;
+
+  & .in-frame-box {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+}
+</style>
