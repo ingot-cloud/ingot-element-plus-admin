211e33618f8f4a800fbd4a31e13908d643777c75
jy <magician.of.technique@aliyun.com>
2025-05-28 16:31:16 +0800

    Fix: unocss

--- unocss.config.ts
+++ unocss.config.ts
@@ -2,34 +2,30 @@ import {
   defineConfig,
   presetAttributify,
   presetIcons,
-  presetUno,
   presetWebFonts,
   transformerDirectives,
 } from "unocss";
+import presetWind3 from "@unocss/preset-wind3";
 import { FileSystemIconLoader } from "@iconify/utils/lib/loader/node-loaders";
 
 export default defineConfig({
-  rules: [
-    [/^line-height-(.+)$/, ([, d]) => ({ "line-height": `${+d / 4}rem` })],
-  ],
+  rules: [[/^line-height-(.+)$/, ([, d]) => ({ "line-height": `${+d / 4}rem` })]],
   shortcuts: [
     ["text-icon", "text-1.4em!"],
     ["color-fade", "text-gray-900:50 dark:text-gray-300:50"],
     ["img-resize", "resize max-w-full h-auto"],
   ],
   presets: [
-    presetUno(),
+    presetWind3(),
     presetAttributify(),
     // 预设 icon，class匹配icon
     presetIcons({
       // cdn: 'https://esm.sh/',
       // When using bundlers, you can provide the collections using dynamic imports so they will be bundler as async chunk and loaded on demand.
       collections: {
-        carbon: () =>
-          import("@iconify-json/carbon/icons.json").then((i) => i.default),
+        carbon: () => import("@iconify-json/carbon/icons.json").then((i) => i.default),
         ep: () => import("@iconify-json/ep/icons.json").then((i) => i.default),
-        mdi: () =>
-          import("@iconify-json/mdi/icons.json").then((i) => i.default),
+        mdi: () => import("@iconify-json/mdi/icons.json").then((i) => i.default),
         ingot: FileSystemIconLoader("./src/assets/icons"),
       },
       scale: 1.4,
