d2d316d0fbb3b64a1fc882c6cfa6955ced811f85
jy <magician.of.technique@aliyun.com>
2025-07-09 11:26:32 +0800

    Fix: 优化InSelect组件

--- src/components/select/InSelect.vue
+++ src/components/select/InSelect.vue
@@ -1,51 +1,39 @@
 <template>
-  <el-select
-    v-model="selectModel"
-    :placeholder="placeholder"
-    :size="size"
-    :multiple="multiple"
-    :clearable="clearable"
-    @change="privateOnChanged"
-  >
+  <el-select v-model="selectModel" @change="privateOnChanged">
     <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
   </el-select>
 </template>
 <script lang="ts" setup>
-import { defineProps, defineEmits, ref, watch } from "vue";
-import { selectProps } from "./props";
+import type { Option } from "@/models";
 
-const props = defineProps(selectProps);
-const emits = defineEmits(["update:modelValue", "onChanged"]);
-
-const selectModel =
-  props.split && props.modelValue
-    ? ref((props.modelValue as string).split(props.split))
-    : ref(props.modelValue);
-
-let selectChange = false;
-let customChange = false;
-
-// 监控 modelValue，如果外层主动改变数值，需要手动改变内部selectModel值
-watch(
-  () => props.modelValue,
-  (nValue) => {
-    if (selectChange) {
-      selectChange = false;
-      return;
-    }
-    customChange = true;
-    selectModel.value = props.split && nValue ? (nValue as string).split(props.split) : nValue;
+const model = defineModel({
+  type: [Array, String, Number, Boolean, Object],
+});
+const props = defineProps({
+  split: {
+    type: String,
+    default: "",
   },
-);
+  options: {
+    type: Array as PropType<Array<Option<any>>>,
+    default() {
+      return [];
+    },
+  },
+});
+const emits = defineEmits(["onChanged"]);
 
-// 监控 selectModel，如果内部选择发生改变，则改变modelValue值
-watch(selectModel, (nValue) => {
-  if (customChange) {
-    customChange = false;
-    return;
-  }
-  selectChange = true;
-  emits("update:modelValue", props.split ? (nValue as []).join(props.split) : nValue);
+const selectModel = computed({
+  set(value: any) {
+    if (props.split && value) {
+      model.value = value.join(props.split);
+    } else {
+      model.value = value;
+    }
+  },
+  get() {
+    return props.split && model.value ? (model.value as string).split(props.split) : model.value;
+  },
 });
 
 const privateOnChanged = (value: any) => {
